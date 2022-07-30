2e094c5c051faac5f563620bb6723815b3a6d606
Ernesto Caballero Delgado <ernesto.cab.del@gmail.com>
2022-07-30 01:28:31 -0500

    add UserMapper interface

--- src/main/java/com/ernesto/mappers/UserMapper.java
+++ src/main/java/com/ernesto/mappers/UserMapper.java
@@ -1,2 +1,15 @@
-package com.ernesto.mappers;public interface UserMapper {
+package com.ernesto.mappers;
+
+import com.ernesto.domain.User;
+import com.ernesto.model.UserCommand;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserCommand userToUserCommand(User user);
+
+    User userCommandToUser(UserCommand userCommand);
 }
